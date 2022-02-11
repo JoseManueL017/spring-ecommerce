82187c80116b07011f91a4c305fae419b6c35490
josema.series <jmom1991@gmail.com>
2022-02-11 17:39:11 +0100

    funcionalidad ver productos para el usuario

--- src/main/java/com/ecommerce/controller/ProductoController.java
+++ src/main/java/com/ecommerce/controller/ProductoController.java
@@ -23,83 +23,83 @@ import org.slf4j.*;
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
-	
+
 	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
-	
+
 	@Autowired
 	private ProductoService productoService;
-	
+
 	@Autowired
 	private UploadFileService upload;
-	
+
 	@GetMapping("")
 	public String show(Model model) {
 		model.addAttribute("productos", productoService.findAll());
 		return "productos/show";
 	}
-	
+
 	@GetMapping("/create")
 	public String create() {
 		return "productos/create";
 	}
-	
+
 	@PostMapping("/save")
-	public String save(Producto producto,@RequestParam("img") MultipartFile file) throws IOException{
+	public String save(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
 		LOGGER.info("Este es el objeto producto {}", producto);
-		Usuario u = new Usuario(1, "", "", "", "", "", "","");
+		Usuario u = new Usuario(1, "", "", "", "", "", "", "");
 		producto.setUsuario(u);
-		
-		//imagen
-		if (producto.getId() == 0) { //cuando sea crea un producto
+
+		// imagen
+		if (producto.getId() == 0) { // cuando sea crea un producto
 			String nombreImagen = upload.saveImages(file);
 			producto.setImagen(nombreImagen);
-		}else {
-		
+		} else {
+
 		}
-		
+
 		productoService.save(producto);
 		return "redirect:/productos";
 	}
-	
+
 	@GetMapping("/edit/{id}")
 	public String edit(@PathVariable Integer id, Model model) {
-		Producto producto= new Producto();
-		Optional<Producto> optionalProducto=productoService.get(id);
-		producto= optionalProducto.get();
-		
+		Producto producto = new Producto();
+		Optional<Producto> optionalProducto = productoService.get(id);
+		producto = optionalProducto.get();
+
 		LOGGER.info("producto buscado: {}", producto);
 		model.addAttribute("producto", producto);
-		
+
 		return "productos/edit";
 	}
-	
+
 	@PostMapping("/update")
 	public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
-		Producto p=new Producto();
-		p=productoService.get(producto.getId()).get();
-		
+		Producto p = new Producto();
+		p = productoService.get(producto.getId()).get();
+
 		if (file.isEmpty()) { // editamos el producto pero no cambiamos la imagen
-			
+
 			producto.setImagen(p.getImagen());
-		}else {
-			
-			//eliminar cuando no sea la imagen por defecto
+		} else {
+
+			// eliminar cuando no sea la imagen por defecto
 			if (!p.getImagen().equals("default.jpg")) {
 				upload.deleteImage(p.getImagen());
 			}
-			
+
 			String nombreImagen = upload.saveImages(file);
 			producto.setImagen(nombreImagen);
 		}
 		producto.setUsuario(p.getUsuario());
 		productoService.update(producto);
 		return "redirect:/productos";
-		
+
 	}
-	
+
 	@GetMapping("/delete/{id}")
 	public String delete(@PathVariable Integer id) {
-		
+
 		productoService.delete(id);
 		return "redirect:/productos";
 	}
