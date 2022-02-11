0b39b40c7ee5f8f980a358f59febbce219300376
josema.series <jmom1991@gmail.com>
2022-02-11 18:16:45 +0100

    mostrar la pista del producto al usuario

--- src/main/java/com/ecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/controller/HomeController.java
@@ -1,9 +1,13 @@
 package com.ecommerce.controller;
 
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ecommerce.service.ProductoService;
@@ -11,6 +15,8 @@ import com.ecommerce.service.ProductoService;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	private final Logger log= LoggerFactory.getLogger(HomeController.class);
 	
 	@Autowired
 	private ProductoService productoService;
@@ -21,6 +27,12 @@ public class HomeController {
 		model.addAttribute("productos", productoService.findAll());
 		
 		return "usuario/home";
+	}
+	
+	@GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id) {
+		log.info("Id enviado como parámetro {}", id);
+		return "usuario/productohome";
 	}
 
 }
